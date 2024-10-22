9eea208d59e1b7873742f1be4f1ecb29efd00fb8
Vishal Juyal <vishaljuyal237@gmail.com>
2024-10-22 23:22:59 +0530

    connecting Database

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
+
 require("dotenv").config();
 const port = process.env.PORT || 3001;
+const db = require("./db");
 const mainRouter = require("./routes/index");
 app.use(express.json());
 
